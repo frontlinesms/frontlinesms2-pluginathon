49eb42b33ac890fe476ce9e7f91f3394dd207dec
ivermac <mark.ekisa@gmail.com>
2012-12-20 10:47:06 +0300

    Added Nexmo Plugin

---
+++ nexmoPlugin/NexmoGrailsPlugin.groovy
@@ -0,0 +1,12 @@
+class NexmoGrailsPlugin {
+    def version = "0.1"
+    def grailsVersion = "2.0 > *"
+    def dependsOn = [:]
+    def pluginExcludes = [
+        "grails-app/views/error.gsp"
+    ]
+    def title = "Nexmo Fconnection Plugin" // Headline display name of the plugin
+    def author = "Mark Ekisa"
+    def authorEmail = ""
+    def description = "This is a Nexmo Fconnection Plugin"
+}
---
+++ nexmoPlugin/test/unit/ketchup/KetchupTests.groovy
@@ -0,0 +1,17 @@
+package ketchup
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Ketchup)
+class KetchupTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
