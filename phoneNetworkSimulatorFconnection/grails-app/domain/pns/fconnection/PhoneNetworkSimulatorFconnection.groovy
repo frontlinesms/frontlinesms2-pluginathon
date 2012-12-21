ec06cb0f14990846271f1d73053c32bd13c198ad
Sitati Kituyi <sitati.kituyi@googlemail.com>
2012-12-21 11:33:59 +0300

    PNS -> Frontline connection

--- phoneNetworkSimulatorFconnection/grails-app/domain/pns/fconnection/PhoneNetworkSimulatorFconnection.groovy
+++ phoneNetworkSimulatorFconnection/grails-app/domain/pns/fconnection/PhoneNetworkSimulatorFconnection.groovy
@@ -2,13 +2,15 @@ package pns.fconnection
 
 import frontlinesms2.camel.*
 import frontlinesms2.*
-
 import org.apache.camel.Exchange
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.model.RouteDefinition
 import frontlinesms2.camel.exception.*
+import frontlinesms2.api.*
 
-class PhoneNetworkSimulatorFconnection extends Fconnection {
+
+@FrontlineApiAnnotations(apiUrl="pns")
+class PhoneNetworkSimulatorFconnection extends Fconnection implements FrontlineApi {
 	static final configFields = [name:null, pnsBaseUrl:null, phoneNumber:null]
 	static final defaultValues = [pnsBaseUrl:'http://localhost:8080/PhoneNetworkSimulator']
 	static passwords = []
@@ -17,10 +19,22 @@ class PhoneNetworkSimulatorFconnection extends Fconnection {
 	String pnsBaseUrl
 	String phoneNumber
 	String name
+	def phoneNetworkSimulatorFconnectionService
     static constraints = {
     	pnsBaseUrl(nullable:false, blank:false)
     	phoneNumber(nullable:false, blank:false)
     }
+
+    //> FrontlineAPI
+    String getSecret() { return "" }
+	def apiProcess(controller) {
+		phoneNetworkSimulatorFconnectionService.apiProcess(this, controller)
+	}
+
+	boolean isApiEnabled() { true }
+	String getFullApiUrl() {
+		return apiEnabled? "http://[your-ip-address]:${appSettingsService.serverPort}/frontlinesms-core/api/1/${PhoneNetworkSimulatorFconnection.getAnnotation(FrontlineApiAnnotations.class)?.apiUrl()}/$id/" : ""
+	}
 
     List<RouteDefinition> getRouteDefinitions() {
 		return new RouteBuilder() {
