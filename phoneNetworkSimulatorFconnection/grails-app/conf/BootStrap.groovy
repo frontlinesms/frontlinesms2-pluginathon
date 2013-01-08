693e3b2c6e6d5c23abe6f142fe57748797142368
Alex Anderson <alex@frontlinesms.com>
2013-01-08 12:57:19 +0300

    Modified Fconnection addition.

--- phoneNetworkSimulatorFconnection/grails-app/conf/BootStrap.groovy
+++ phoneNetworkSimulatorFconnection/grails-app/conf/BootStrap.groovy
@@ -1,17 +1,11 @@
 import pns.*
 import pns.fconnection.*
 import frontlinesms2.*
+import pns.fconnection.PhoneNetworkSimulatorFconnection
 
 class BootStrap {
-
 	def init = { servletContext ->
-		def oldImplementations = frontlinesms2.Fconnection.implementations
-		frontlinesms2.Fconnection.metaClass.static.getImplementations = { ->
-			if (!oldImplementations.contains(pns.fconnection.PhoneNetworkSimulatorFconnection))
-				oldImplementations = (oldImplementations << pns.fconnection.PhoneNetworkSimulatorFconnection)
-			println "AVAILABLE FCONNECTIONS: ${oldImplementations}}"
-			return oldImplementations
-		}
+		Fconnection.implementations << PhoneNetworkSimulatorFconnection
 
 		new PhoneNetworkSimulatorFconnection(name:"My PNS", pnsBaseUrl:'http://192.168.0.106:8888/PhoneNetworkSimulator', phoneNumber:'000').save(failOnError:true, flush:true)
 	}
@@ -19,3 +13,4 @@ class BootStrap {
 	def destroy = {
 	}
 }
+
